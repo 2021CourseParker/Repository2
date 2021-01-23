bff0a114b25e9ea08676adb8edc759e80d358417
Milos <dzavid.bestija@gmail.com>
2021-01-23 10:05:12 +0100

    Added Monster

---
+++ PracticeApp/PracticeApp/Monster.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeApp
+{
+    class Monster
+    {
+        public int Health { get; set; }
+        public int Experience { get; set; } // how much experience this monster gives when killed
+
+        public void ShowInformation()
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Monster info: ");
+            Console.WriteLine("Health: " + Health);
+            Console.WriteLine("Experience: " + Experience);
+            Console.WriteLine("-------------------------------------");            
+        }
+    }
+}
--- PracticeApp/PracticeApp/Program.cs
+++ PracticeApp/PracticeApp/Program.cs
@@ -7,43 +7,10 @@ namespace PracticeApp
     {
         static void Main(string[] args)
         {
-            // VAR
-            // METHODS
-            // ARRAYS
-            // LIST
-            // CLASS
-
-
-            int[] someArray = new int[5];
-                            
-            
-            int counter = 0;
-
-            for (int i = 0; i < someArray.Length; i++)
-            {
-                someArray[i] = i;                
-            }
-
-
-            for (int i = 0; i < someArray.Length; i++)
-            {
-                Console.WriteLine(someArray[i]);
-            }
-
-            List<int> someData = new List<int>();
-
-
-
-            for (int i = 0; i < 5; i++)
-            {
-                someData.Add(i);
-            }
-
-
-            foreach (var item in someData)
-            {
-                Console.WriteLine(item.ToString());
-            }
+            Monster monster = new Monster();
+            monster.Health = 10;
+            monster.Experience = 5;
+            monster.ShowInformation();
 
 
             Console.Read();
