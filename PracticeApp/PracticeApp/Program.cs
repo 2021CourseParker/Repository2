b7e79e63b23e85102c46c8a910e58fd986fb8699
Milos <dzavid.bestija@gmail.com>
2021-01-23 09:21:17 +0100

    AddingArray

--- PracticeApp/PracticeApp/Program.cs
+++ PracticeApp/PracticeApp/Program.cs
@@ -6,7 +6,30 @@ namespace PracticeApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // VAR
+            // METHODS
+            // ARRAYS
+            // LIST
+            // CLASS
+
+
+            int[] someArray = new int[5];
+                            
+            
+            int counter = 0;
+
+            for (int i = 0; i < someArray.Length; i++)
+            {
+                someArray[i] = i;                
+            }
+
+
+            for (int i = 0; i < someArray.Length; i++)
+            {
+                Console.WriteLine(someArray[i]);
+            }
+          
+            
 
             Console.Read();
         }
