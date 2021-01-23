2efdda31026253d88b7ab5a4d026b8bea87ced07
Milos <dzavid.bestija@gmail.com>
2021-01-23 08:47:36 +0100

    Initial commit

---
+++ PracticeApp/PracticeApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PracticeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Read();
+        }
+    }
+}
